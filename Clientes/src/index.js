09b6aebe75cea2b48c72ad7f5109778177b4d199
Cristian <isrunaway@gmail.com>
2021-03-25 16:43:51 -0300

    Clientes

---
+++ Clientes/src/index.js
@@ -0,0 +1,71 @@
+const inputNombre = document.getElementById('nombre');
+const inputDni = document.getElementById('dni');
+const inputDolar = document.getElementById('dolar');
+const botonAgregar = document.getElementById('agregar');
+const labelError = document.getElementById('error');
+const listarClientes = document.getElementById('listar-clientes');
+let listaClientes = [];
+
+botonAgregar.addEventListener("click", agregarCliente);
+
+document.addEventListener("click", eliminarCliente);
+
+function agregarCliente(event){
+    event.preventDefault();
+    labelError.innerHTML = '';
+    const repiteDNI =  listaClientes.some((valor) => valor.dni === inputDni.value);
+    const error = podesAvanzar(repiteDNI);
+    if(error)
+        escribirError(error);
+    else{
+        const clave = Math.floor(Math.random() * 50000);
+        listaClientes.push({
+            id: clave,
+            dni: inputDni.value,
+            nombre: inputNombre.value,
+            dolar: inputDolar.value,
+        });
+        escribirHtml();
+    }
+
+    blanquear();
+}
+
+function eliminarCliente(event){
+    if (event.target.id === "eliminar") {
+        const idABuscar = event.target.parentElement.attributes.key.value;
+        const nuevoArray = listaClientes.filter(
+          (valor) => valor.id !== Number(idABuscar)
+        );
+        listaClientes = nuevoArray;
+        escribirHtml();
+      }
+}
+
+function podesAvanzar(flag){
+    let error = 0;
+    if(flag)
+    error = 1;
+    else if(inputDni.value ==='' || inputNombre.value ==='')
+        error = 2;
+    return error;    
+}
+function blanquear(){
+    inputDni.value = '';
+    inputNombre.value = '';
+}
+function escribirError(tipo){
+    if(tipo == 1)
+        labelError.innerHTML = "El DNI ya existe";
+    if(tipo == 2)
+        labelError.innerHTML = "Revise los campos DNI y Nombre";
+}
+
+function escribirHtml(){
+     const nuevoArray = listaClientes.map(
+        (valor) =>
+        `<li key=${valor.id}> <div class="item">${valor.id}</div> <div class="item">${valor.dni}</div> <div class="item">${valor.nombre}</div> <div class="item">${valor.dolar}</div> <button id="eliminar">Eliminar</button></li>`
+      );
+      const nuevoArrayJoineado = nuevoArray.join("");
+      listarClientes.innerHTML = `<li> <div class="item-titulo">CLAVE</div> <div class="item-titulo">DNI</div> <div class="item-titulo">NOMBRES</div> <div class="item-titulo">U$S</div> <div class="item-titulo">Acción</div> </li>` + nuevoArrayJoineado;
+}
