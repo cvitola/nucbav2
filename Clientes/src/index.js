d01676a575faac1592538aeb91fb74ee9d8a021a
Cristian <isrunaway@gmail.com>
2021-03-30 13:40:15 -0300

    agrega busqueda cliente

--- Clientes/src/index.js
+++ Clientes/src/index.js
@@ -4,12 +4,22 @@ const inputDolar = document.getElementById('dolar');
 const botonAgregar = document.getElementById('agregar');
 const labelError = document.getElementById('error');
 const listarClientes = document.getElementById('listar-clientes');
+const inputCliente = document.getElementById('cliente');
+const listarBusqueda = document.getElementById('listar-busqueda');
 let listaClientes = [];
 
 botonAgregar.addEventListener("click", agregarCliente);
-
 document.addEventListener("click", eliminarCliente);
+inputCliente.addEventListener('keyup', buscarClientes); //por cada vez que hace clic en el input
+//botonBuscar.addEventListener("click", buscarClientes);
 
+function buscarClientes(){
+    
+    const clienteABuscar = inputCliente.value.toUpperCase();
+    const nuevoArray = listaClientes.filter( element => element.nombre.toUpperCase().includes(clienteABuscar)  )
+    console.log(nuevoArray)
+    escribirHtml(nuevoArray, listarBusqueda);
+}
 function agregarCliente(event){
     event.preventDefault();
     labelError.innerHTML = '';
@@ -25,7 +35,7 @@ function agregarCliente(event){
             nombre: inputNombre.value,
             dolar: inputDolar.value,
         });
-        escribirHtml();
+        escribirHtml(listaClientes, listarClientes);
     }
 
     blanquear();
@@ -45,7 +55,7 @@ function eliminarCliente(event){
 function podesAvanzar(flag){
     let error = 0;
     if(flag)
-    error = 1;
+        error = 1;
     else if(inputDni.value ==='' || inputNombre.value ==='')
         error = 2;
     return error;    
@@ -61,11 +71,11 @@ function escribirError(tipo){
         labelError.innerHTML = "Revise los campos DNI y Nombre";
 }
 
-function escribirHtml(){
-     const nuevoArray = listaClientes.map(
+function escribirHtml(unaLista, elementoUl){
+     const nuevoArray = unaLista.map(
         (valor) =>
         `<li key=${valor.id}> <div class="item">${valor.id}</div> <div class="item">${valor.dni}</div> <div class="item">${valor.nombre}</div> <div class="item">${valor.dolar}</div> <button id="eliminar">Eliminar</button></li>`
       );
       const nuevoArrayJoineado = nuevoArray.join("");
-      listarClientes.innerHTML = `<li> <div class="item-titulo">CLAVE</div> <div class="item-titulo">DNI</div> <div class="item-titulo">NOMBRES</div> <div class="item-titulo">U$S</div> <div class="item-titulo">Acción</div> </li>` + nuevoArrayJoineado;
+      elementoUl.innerHTML = `<li> <div class="item-titulo">CLAVE</div> <div class="item-titulo">DNI</div> <div class="item-titulo">NOMBRES</div> <div class="item-titulo">U$S</div> <div class="item-titulo">Acción</div> </li>` + nuevoArrayJoineado;
 }
