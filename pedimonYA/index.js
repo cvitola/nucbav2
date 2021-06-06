4ccc443f33e0a257e464cbd948f16de427891715
Cristian <isrunaway@gmail.com>
2021-06-06 19:44:13 -0300

    carro y responsive

--- pedimonYA/index.js
+++ pedimonYA/index.js
@@ -1,48 +1,43 @@
 const listaPokemons = document.getElementById('grilla');
 const carrito = document.getElementById('carro-poke');
 const botonCarrito = document.getElementById('ver-carrito');
+const numerito = document.getElementById("numerito");
 let precioCarrito=0;
+let listaPokemones = []; //los que levanto del JSON
 let carritoPokes = [];
+
+
 function levantarDatos() {
     fetch('poke.json')
         .then(resp => resp.json())
         .then(resp => {dibujarTarjetas(resp)})
 }
 
-function recuperarIDPoke(idDelPokemon){
-    fetch('poke.json')
-    .then(resp => resp.json())
-    .then(resp => {
-        const obj = resp.filter(
-            (valor) => valor.idPokemon === Number(idDelPokemon)
-        )
-        carritoPokes.push(obj[0]);
-        actualizarMonto(obj[0].importe)
-        dibujarTarjetasEnCarrito(carritoPokes)
-    });   
-}
+
+
 levantarDatos();
+
 function dibujarTarjetas(pokemones){
     const tarjetasHtml = pokemones.map((objeto) => {
+        listaPokemones.push(objeto);
         if(objeto.cantidad != 0){
             return `<article key=${objeto.idPokemon} class="card-poke">
- 
             <img src=${objeto.urlImagen} alt=${objeto.nombre}>
-            <p class="nombre-poke">${objeto.nombre}</p>
+            <p class="">${objeto.nombre}</p>
             <p class="precio">$ ${objeto.importe}</p>
-            <p class="precio">${objeto.peso} kg</p>
-            <p class="precio">${objeto.tipo}</p>
-            <button id="agregar" class="btn">COMPRAR</button>
+            <p class="">${objeto.peso} kg</p>
+            <p class="">${objeto.tipo}</p>
+            <button id="agregar" class="btn btn-grad">COMPRAR</button>
         <article>`
         }else{
             return `<article key=${objeto.idPokemon} class="card-poke">
             <p class="sin-stock">SIN STOCK</p>
             <img src=${objeto.urlImagen} alt=${objeto.nombre}>
-            <p class="nombre-poke">${objeto.nombre}</p>
+            <p class="">${objeto.nombre}</p>
             <p class="precio">$ ${objeto.importe}</p>
-            <p class="precio">${objeto.peso} kg</p>
-            <p class="precio">${objeto.tipo}</p>
-            <button id="agregar" disabled class="btn">COMPRAR</button>
+            <p class="">${objeto.peso} kg</p>
+            <p class="">${objeto.tipo}</p>
+            <button id="agregar" disabled class="btn btn-grad">COMPRAR</button>
         <article>`
         }
 
@@ -71,10 +66,14 @@ function dibujarTarjetasEnCarrito(listitaCompra){
     carrito.innerHTML = arrayJoineado;
 }
 
-function actualizarMonto(importe){
-    precioCarrito+=importe;
+function actualizarMonto(){
+    let sumatoria = 0;
+    carritoPokes.forEach((elemento) => {
+        sumatoria+=elemento.importe;
+    })
     const precioHtml = document.getElementById("precioCarrito");
-    precioHtml.innerHTML = `$ ${precioCarrito}`;
+    precioHtml.innerHTML = `$ ${sumatoria}`;
+    numerito.innerHTML = `${carritoPokes.length}`;
 }
 
 document.addEventListener("click", comprarPokemon);
@@ -95,12 +94,49 @@ function visualizarCarrito(){
 function comprarPokemon(event){
     if (event.target.id ==="agregar"){
         const idPoke = event.target.parentElement.attributes.key.value; //recupero el ID del poKe
-        recuperarIDPoke(idPoke)
+        //recuperarIDPoke(idPoke)
+        obtenerPokemon(idPoke)
     }
 }
+
+function obtenerPokemon(idDelPokemon){
+    if(!estasEnCarrito(idDelPokemon)){
+        const pokeAlCarrito = listaPokemones.filter(
+            (element) => element.idPokemon === Number(idDelPokemon)
+        )
+        carritoPokes.push(pokeAlCarrito[0]);
+        actualizarMonto();
+        dibujarTarjetasEnCarrito(carritoPokes)
+    }else{
+        alert("Ese Poke ya está en el carrito");
+    }
+
+}
+
+    /*fetch('poke.json')
+    .then(resp => resp.json())
+    .then(resp => {
+        const obj = resp.filter(
+            (valor) => valor.idPokemon === Number(idDelPokemon)
+        )*/  
+
+
+function estasEnCarrito(id){
+   return carritoPokes.some((val) => val.idPokemon === Number(id));
+}
+
 function quitarPokemon(event){
     if(event.target.id === "quitar"){
         const idPoke = event.target.parentElement.attributes.key.value; //recupero el ID del poKe
         quitarPokemonDelCarro(idPoke);
     }
+}
+
+function quitarPokemonDelCarro(identificador){
+    const nuevoArray = carritoPokes.filter(
+          (valor) => valor.idPokemon !== Number(identificador)
+        );
+        carritoPokes = nuevoArray;
+        actualizarMonto()
+        dibujarTarjetasEnCarrito(carritoPokes);
 }
